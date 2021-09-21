81cff3649eefceddf21769907ba6fa06bff64acc
vozmak <strela996@bk.ru>
2021-09-21 12:49:50 +0300

    refactor: Change usage app.METHOR to express.Router() and exports router

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,14 @@
+import { NextFunction, Request, Response } from 'express';
 import * as express from 'express';
 import * as cors from 'cors';
 import * as swaggerUi from 'swagger-ui-express';
 import * as YAML from 'yamljs';
-import * as fileUpload from 'express-fileupload'
-import {routes} from './routes/routes'
-import {logger} from "./logger/logger";
-import {authorizationChecker} from "./middleware/authorizationChecker";
+import * as fileUpload from 'express-fileupload';
+import { logger } from './logger/logger';
+import { authorizationChecker } from './middleware/authorizationChecker';
+import loginRouter from './routes/loginRouter';
+import displayGalleryRouter from './routes/displayGalleryRouter';
+import addImgRouter from './routes/addImgRouter';
 
 const app = express();
 const PORT: number = 2000;
@@ -13,7 +16,7 @@ const hostname: string = '127.0.0.1';
 const swaggerDocument = YAML.load(`server/swagger/swaggerAPI.yaml`);
 
 app.use(cors({
-    origin: '*',
+  origin: '*',
 }));
 app.use(express.json());
 app.all('*', authorizationChecker);
@@ -22,9 +25,18 @@ app.use(express.static(`${__dirname}/gallery/images`));
 app.use(fileUpload());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-routes(app);
+app.use('/authorization', loginRouter);
+app.use('/gallery/:page', displayGalleryRouter);
+app.use('/gallery/:page', addImgRouter);
+
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+  res.writeHead(404);
+  res.end('Not Found');
+  logger(`Page ${req.url} not found`);
+  next();
+});
 
 app.listen(PORT, hostname, () => {
-    console.log(`Listening server: ${hostname}:${PORT}`);
-    logger('Server start')
+  console.log(`Listening server: ${hostname}:${PORT}`);
+  logger('Server start');
 });
